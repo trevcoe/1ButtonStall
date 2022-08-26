2bd1a01a9dfb6b3571632f455b03184ccdce36b4
Trevor Coe <88930059+trevcoe@users.noreply.github.com>
2022-08-26 05:28:13 -0400

    Add files via upload

--- 1buttonstall.py
+++ 1buttonstall.py
@@ -12,7 +12,7 @@ airRoll = "RollRight"
 # If you chose RollRight, type YawLeft. If you chose RollLeft, type YawRight. Leave the quotes. Caps matter.
 yaw = "YawLeft"
 
-
+# Don't touch anything below this line.
 with open(r'C:\Users\trevy\Documents\My Games\Rocket League\TAGame\Config\TAInput.ini', "r") as in_file:
     buf = in_file.readlines()
 
@@ -23,6 +23,7 @@ with open(r'C:\Users\trevy\Documents\My Games\Rocket League\TAGame\Config\TAInpu
             line = line + stall
         out_file.write(line)
 
+# Reference code. This is how it should look in the config.
 
 #GamepadBindings=( Action="YawLeft",Key="XboxTypeS_X", bRequired=true )AxisSign=AxisSign_Negative, )
 #GamepadBindings=( Action="RollRight",Key="XboxTypeS_X", bRequired=true )
